7217cb1255b7b613e77ab5a9d1934fc614d1f668
Awais Butt <awaisb209@gmail.com>
2022-09-13 22:58:36 +0500

    STY

--- app/src/main/java/com/example/sty/MainActivity.java
+++ app/src/main/java/com/example/sty/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.sty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btn1,btn2,btn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+btn1.findViewById(R.id.btnmen);
+btn2.findViewById(R.id.btnwomen);
+btn3.findViewById(R.id.btnacsesori);
 
     }
 }
