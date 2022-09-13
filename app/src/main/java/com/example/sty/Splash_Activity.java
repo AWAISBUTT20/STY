f5285cd2c4f260be806b1afeb324ff2a88e9f7c1
Awais Butt <awaisb209@gmail.com>
2022-09-13 14:37:27 +0500

    STYMEUP

---
+++ app/src/main/java/com/example/sty/Splash_Activity.java
@@ -0,0 +1,24 @@
+package com.example.sty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Intent nxt = new Intent(this,MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(nxt);
+                finish();
+            }
+        },2000);
+    }
+}
