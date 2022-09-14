e0c16e383bf6b2a48c021feabf179e27bb49554a
Awais Butt <awaisb209@gmail.com>
2022-09-14 16:31:37 +0500

    STY

--- app/src/main/java/com/example/sty/MainActivity.java
+++ app/src/main/java/com/example/sty/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.sty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-Button btn1,btn2,btn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-btn1.findViewById(R.id.btnmen);
-btn2.findViewById(R.id.btnwomen);
-btn3.findViewById(R.id.btnacsesori);
-
-    }
+}
 }
---
+++ app/src/main/java/com/example/sty/mens_activity.java
@@ -0,0 +1,14 @@
+package com.example.sty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mens_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mens_avtivity);
+    }
+}
