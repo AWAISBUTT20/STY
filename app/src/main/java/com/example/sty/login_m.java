a44c69964622604284fb219548d5c041940fab9c
Awais Butt <awaisb209@gmail.com>
2022-09-15 22:07:18 +0500

    STY

---
+++ app/src/main/java/com/example/sty/login_m.java
@@ -0,0 +1,15 @@
+package com.example.sty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_m extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_m);
+
+    }
+}
