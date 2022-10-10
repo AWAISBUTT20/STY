64d84550a2f4d6cade6889eb3d659b5f962f25b7
Awais Butt <awaisb209@gmail.com>
2022-10-10 10:50:57 +0500

    STY

--- app/src/main/java/com/example/sty/Cart.java
+++ app/src/main/java/com/example/sty/Cart.java
@@ -1,5 +1,6 @@
 package com.example.sty;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class Cart extends Fragment {
@@ -22,6 +24,16 @@ public class Cart extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false);
+        View view = inflater.inflate(R.layout.fragment_cart, container, false);
+        Button btnOpen = (Button) view. findViewById(R.id.btncheckout);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent in = new Intent(getActivity(), checkout.class);
+                //in.putExtra("some", "Mens Wears");
+                startActivity(in);
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/sty/mens_activity.java
+++ app/src/main/java/com/example/sty/mens_activity.java
@@ -2,15 +2,26 @@ package com.example.sty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class mens_activity extends AppCompatActivity {
-
+Button btn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mens_avtivity);
+        btn3 = findViewById(R.id.btnmentees);
+        Intent intent=new Intent(mens_activity.this,mens_product.class);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             if (bundle.getString("some") != null) {
