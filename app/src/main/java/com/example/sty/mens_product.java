c45418d97686730dc8bb12c55abc904d7aa86bb2
Awais Butt <awaisb209@gmail.com>
2022-10-26 22:25:27 +0500

    STYmeup

--- app/src/main/java/com/example/sty/mens_product.java
+++ app/src/main/java/com/example/sty/mens_product.java
@@ -1,14 +1,52 @@
 package com.example.sty;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.GridLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class mens_product extends AppCompatActivity {
-
+    RecyclerView rv;
+    List<String> description = new ArrayList<>();
+    List<Integer> Product = new ArrayList<>();
+    Adapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mens_product);
+        rv=findViewById(R.id.recyclerView);
+        //description
+        description.add("Product Name 1 \n Description 1 \n Price 10$");
+        description.add("Product Name 2 \n Description 2 \n Price 10$");
+        description.add("Product Name 3 \n Description 3 \n Price 10$");
+        description.add("Product Name 4 \n Description 4 \n Price 10$");
+        description.add("Product Name 5 \n Description 5 \n Price 10$");
+        description.add("Product Name 6 \n Description 6 \n Price 10$");
+        description.add("Product Name 7 \n Description 7 \n Price 10$");
+        description.add("Product Name 8 \n Description 8 \n Price 10$");
+        description.add("Product Name 9 \n Description 9 \n Price 10$");
+        description.add("Product Name 10 \n Description 10 \n Price 10$");
+        //product picture
+        Product.add(R.drawable.menhodies);
+        Product.add(R.drawable.mentees);
+        Product.add(R.drawable.womenmodle);
+        Product.add(R.drawable.female);
+        Product.add(R.drawable.womenhood);
+        Product.add(R.drawable.womentees);
+        Product.add(R.drawable.product);
+        Product.add(R.drawable.neck);
+        Product.add(R.drawable.caps);
+        Product.add(R.drawable.acsesories);
+
+        myadapter adapter=new myadapter(description,Product,this);
+        GridLayoutManager gridLayoutManager=new GridLayoutManager(this,2, GridLayoutManager.VERTICAL,false);
+        rv.setLayoutManager(gridLayoutManager);
+        rv.setAdapter((RecyclerView.Adapter) adapter);
     }
 }
---
+++ app/src/main/java/com/example/sty/myadapter.java
@@ -0,0 +1,53 @@
+package com.example.sty;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class myadapter extends RecyclerView.Adapter<myadapter.ViewHolder> {
+    List<String> desc;
+    List<Integer> product;
+    LayoutInflater inflater;
+
+    public myadapter(List<String> desc, List<Integer> image, Context context) {
+        this.desc = desc;
+        this.product = image;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.product, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.txtdesc.setText(desc.get(position));
+        holder.prdct.setImageResource(product.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+return desc.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txtdesc;
+        ImageView prdct;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtdesc = itemView.findViewById(R.id.textView);
+            prdct = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
