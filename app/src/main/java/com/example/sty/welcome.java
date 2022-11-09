3c39601382844cb9950fdd4aee7dbf7b86fb4203
Awais Butt <awaisb209@gmail.com>
2022-11-09 11:08:57 +0500

    STY

--- app/src/main/java/com/example/sty/welcome.java
+++ app/src/main/java/com/example/sty/welcome.java
@@ -6,31 +6,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
-
-
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.Menu;
+import android.view.View;
 
 public class welcome extends AppCompatActivity {
-//Button btn1,btn2,btn3;
-//FirebaseAuth firebaseAuth;
+    Button btn1, btn2, btn3;
+   // FirebaseAuth firebaseAuth;
     NavigationView navigationView;
     ActionBarDrawerToggle toggle;
     DrawerLayout drawerLayout;
     Toolbar toolbar;
-/*
+
+    /*firebase
     @Override
     protected void onStart() {
         super.onStart();
@@ -38,44 +31,41 @@ public class welcome extends AppCompatActivity {
         if (user == null) {
             startActivity(new Intent(welcome.this,login_m.class));
         }
-    }
-*/
-
-    @SuppressLint("MissingInflatedId")
+    }*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wellcome);
-        toolbar = findViewById(R.id.upertoolbar);
-        setSupportActionBar(toolbar);
-        navigationView = findViewById(R.id.nagationmenu);
+
+        toolbar = findViewById(R.id.custom_toolbar);
+        //setSupportActionBar(toolbar);
+
+        navigationView = findViewById(R.id.navigationmenu);
         drawerLayout = findViewById(R.id.drawer);
-        toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open,R.string.close);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
                 switch (item.getItemId()){
                     case R.id.Mensmenu:
-                        Toast.makeText(welcome.this, "Mens Clicked", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(welcome.this, "Home Clicked", Toast.LENGTH_SHORT).show();
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
 
                     case R.id.Womensmenu:
-                        Toast.makeText(welcome.this, "Women Clicked", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(welcome.this, "Setting Clicked", Toast.LENGTH_SHORT).show();
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
 
-                    case R.id.accsesorymenu:
-                        Toast.makeText(welcome.this, "Accessories Clicked", Toast.LENGTH_SHORT).show();
-                        drawerLayout.closeDrawer(GravityCompat.START);
-                        break;
                     case R.id.logoutmenu:
-                        Toast.makeText(welcome.this, "Logout Clicked", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(welcome.this, "Exit Clicked", Toast.LENGTH_SHORT).show();
                         drawerLayout.closeDrawer(GravityCompat.START);
                         break;
+
                     default:
                         Toast.makeText(welcome.this, "Kuch Ni", Toast.LENGTH_SHORT).show();
                 }
@@ -83,35 +73,67 @@ public class welcome extends AppCompatActivity {
                 return true;
             }
         });
-       /*
-        btn1 =findViewById(R.id.btnwelcomelog);
-        btn2 =findViewById(R.id.btnwelcomesignup);
-        btn3 =findViewById(R.id.btnwellcomayb);
-        firebaseAuth =FirebaseAuth.getInstance();
+        //Activity
+        btn1 = findViewById(R.id.btnwelcomelog);
+        btn2 = findViewById(R.id.btnwelcomesignup);
+        btn3 = findViewById(R.id.btnwellcomayb);
+        //firebaseAuth = FirebaseAuth.getInstance();
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(welcome.this,login_m.class);
+                Intent intent = new Intent(welcome.this, login_m.class);
                 startActivity(intent);
             }
         });
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(welcome.this,SignUp.class);
+                Intent intent = new Intent(welcome.this, SignUp.class);
                 startActivity(intent);
             }
         });
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(welcome.this,MainActivity.class);
+                Intent intent = new Intent(welcome.this, MainActivity.class);
                 startActivity(intent);
             }
-        });*/
-
+        });
 
     }
 
+   /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.Mensmenu:
+                Intent intent=new Intent(welcome.this,mens_activity.class);
+                Toast.makeText(welcome.this, "Mens Clicked", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+
+            case R.id.Womensmenu:
+                Toast.makeText(welcome.this, "Women Clicked", Toast.LENGTH_SHORT).show();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+
+            case R.id.accsesorymenu:
+                Toast.makeText(welcome.this, "Accessories Clicked", Toast.LENGTH_SHORT).show();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.logoutmenu:
+                Toast.makeText(welcome.this, "Logout Clicked", Toast.LENGTH_SHORT).show();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+            default:
+                Toast.makeText(welcome.this, "Kuch Ni", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }*/
 }
