d76704066748ef49c426d17adf5094962de59dba
Awais Butt <awaisb209@gmail.com>
2022-12-07 22:38:52 +0500

    STY

--- app/src/main/java/com/example/sty/Home.java
+++ app/src/main/java/com/example/sty/Home.java
@@ -10,6 +10,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.denzcoskun.imageslider.ImageSlider;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
 public class Home extends Fragment {
 
     public Home() {
@@ -22,6 +29,15 @@ public class Home extends Fragment {
         // Inflate the layout for this fragment
 
       View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ImageSlider imageSlider=view.findViewById(R.id.imageslider);
+
+        List<SlideModel> slideModels=new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.mentees,ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.womentees,ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.menhodies,ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.womenhood,ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.acsesories,ScaleTypes.FIT));
+        imageSlider.setImageList(slideModels, ScaleTypes.FIT);
           Button btnOpen = (Button) view. findViewById(R.id.btnmen);
         btnOpen.setOnClickListener(new View.OnClickListener() {
             @Override
