d3e7e6f61217858b24f84d207fdbe3d2db165a93
Awais Butt <awaisb209@gmail.com>
2022-12-30 22:19:34 +0500

    STY

--- app/src/main/java/com/example/sty/Home.java
+++ app/src/main/java/com/example/sty/Home.java
@@ -16,7 +16,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import com.denzcoskun.imageslider.ImageSlider;
@@ -28,7 +31,7 @@ import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 
 public class Home extends Fragment {
-
+Animation top;
     public Home() {
         // Required empty public constructor
     }
@@ -38,14 +41,17 @@ public class Home extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        Button btnOpen = (Button) view.findViewById(R.id.btnmen);
+        top= AnimationUtils.loadAnimation(getContext(),R.anim.top_animation);
+        TextView txt1 = view.findViewById(R.id.txtnewcollection);
+        txt1.setAnimation(top);
+       /* Button btnOpen = (Button) view.findViewById(R.id.btnmen);
         btnOpen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent in = new Intent(getActivity(), mens_activity.class);
                 startActivity(in);
             }
-        });
+        });*/
         Button btnOpen2 = (Button) view.findViewById(R.id.btnwomen);
         btnOpen2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +68,19 @@ public class Home extends Fragment {
                 startActivity(in);
             }
         });
+
+        //image slider
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        ImageSlider imageSlider= view.findViewById(R.id.homeImgSlide);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.acsesories, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.acsesories, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.acsesories, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.acsesories, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.acsesories, ScaleTypes.FIT));
+        imageSlider.setImageList(slideModels, ScaleTypes.CENTER_INSIDE);
+        imageSlider.startSliding(2000);
+
         return view;
     }
 
